2fc29a178fe707da43fc19fd2cf184f84bb50a67
VISHNU-P-M <vishnuputhanalakkal@gmail.com>
2021-03-07 19:44:19 +0530

    allowed host changed

--- hotelmanagement/hotelmanagement/settings.py
+++ hotelmanagement/hotelmanagement/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fp=xf8%3pjd@4@vb(f*qfkq59ihl)c=u$v@m-e74u+gz1_0&&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
